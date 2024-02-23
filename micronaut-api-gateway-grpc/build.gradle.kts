4962269cb0a3cea19306aa0500d9ce6c3c41cba3
Le Gall, Benoit <benoit.legall@rakuten.com>
2024-02-23 10:28:30 +0100

    upgrade Micronaut version

--- micronaut-api-gateway-grpc/build.gradle.kts
+++ micronaut-api-gateway-grpc/build.gradle.kts
@@ -1,17 +1,19 @@
 import com.google.protobuf.gradle.id
 
 plugins {
-    id("io.micronaut.minimal.library") version "4.2.1"
+    id("io.micronaut.minimal.library") version "4.3.3"
     id("com.google.protobuf") version "0.9.3"
 }
 
 // todo how to be sure to use the same versions than the ones defined in "micronaut-grpc-bom" (grpc.version & protobuf.version) ?
-// micronaut-bom:4.1.2 -> micronaut-grpc-bom:4.0.1
-// see https://central.sonatype.com/artifact/io.micronaut.platform/micronaut-platform/4.1.2
-// see https://central.sonatype.com/artifact/io.micronaut.grpc/micronaut-grpc-bom/4.0.1
-val protobuf = "3.23.4"
-val grpc = "1.56.1"
+// micronaut-bom:4.2.1 -> micronaut-grpc-bom:4.0.1
+// see https://central.sonatype.com/artifact/io.micronaut.platform/micronaut-platform/4.2.1
+// see https://central.sonatype.com/artifact/io.micronaut.grpc/micronaut-grpc-bom/4.2.0
+val protobuf = "3.25.1"
+val grpc = "1.59.1"
 val lombokVersion = "1.18.24"
+val mockitoInline = "5.2.0"
+val grpcmock = "0.10.1"
 
 dependencies {
     compileOnly("org.projectlombok:lombok:$lombokVersion")
@@ -33,15 +35,14 @@ dependencies {
 
     testImplementation("io.micronaut.test:micronaut-test-junit5")
     testImplementation("org.assertj:assertj-core")
-    testImplementation("org.mockito:mockito-inline:5.2.0")
+    testImplementation("org.mockito:mockito-inline:$mockitoInline")
     testImplementation("org.mockito:mockito-junit-jupiter")
-    testImplementation("org.grpcmock:grpcmock-junit5:0.10.1")
+    testImplementation("org.grpcmock:grpcmock-junit5:$grpcmock")
     testImplementation("io.grpc:grpc-testing")
 
     testRuntimeOnly("ch.qos.logback:logback-classic")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     testRuntimeOnly("org.yaml:snakeyaml")
-
 
     testImplementation(enforcedPlatform("io.grpc:grpc-bom:${grpc}"))
     testRuntimeOnly(enforcedPlatform("io.grpc:grpc-bom:${grpc}"))
