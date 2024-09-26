74288dc106ca5633e01eb5dd1d85348699de6829
Le Gall, Benoit <legall.benoit@gmail.com>
2024-09-26 22:29:24 +0200

    [clean-up] Improve dependencies

--- micronaut-api-gateway-http/build.gradle.kts
+++ micronaut-api-gateway-http/build.gradle.kts
@@ -9,12 +9,14 @@ dependencies {
     annotationProcessor(mn.micronaut.validation.processor)
 
     implementation(mn.micronaut.cache.caffeine)
-    implementation(mn.micronaut.http.client.asProvider())
+    implementation(mn.micronaut.http.client)
     implementation(mn.micronaut.management)
-    implementation(mn.micronaut.reactor.asProvider())
+    implementation(mn.micronaut.reactor)
     implementation(mn.micronaut.reactor.http.client)
     implementation(mn.micronaut.serde.jackson)
-    implementation(mn.micronaut.validation.asProvider())
+    implementation(mn.micronaut.validation)
+
+    runtimeOnly(mn.micronaut.discovery.client)
 
     testImplementation(mn.micronaut.http.server.netty)
     testImplementation(mn.micronaut.test.rest.assured)
